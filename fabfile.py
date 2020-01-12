385d5d9b7385a5b5294a70f34dd76053e047b5c5
poopsplat <matt.m.soria@gmail.com>
2020-01-11 18:56:59 -0600

    Updating fabfile for opalstack server

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,10 @@
 from fabric.api import *
 import os
 
-env.hosts = ['mattsoria.com']
-env.user = 'poopsplat'
+env.hosts = ['opal4.opalstack.com']
+env.user = 'soriamatt'
 env.path = '~/Sites/ms2016'
-env.remotepath = '/home/poopsplat/webapps/ms2016'
+env.remotepath = '/home/soriamatt/apps/ms2016'
 env.git_branch = 'master'
 env.warn_only = True
 env.remote_protocol = 'http'
